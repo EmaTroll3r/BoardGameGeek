8bd145d91321de38ba864115e4e3daf949e18061
EmaTroll3r <emanuelecostanzo03@gmail.com>
2025-09-28 22:09:05 +0200

    sistemato formatPublishDate

--- public/scripts/common.js
+++ public/scripts/common.js
@@ -9,27 +9,5 @@ function onResponseReturnText(response) {
 }
 
 function formatPublishDate(dateString) {
-    const date = new Date(dateString);
-    const now = new Date();
-    const diffMs = now - date;
-    const seconds = parseInt(diffMs / 1000);
-
-    const units = [
-        { name: "year",   value: 60 * 60 * 24 * 365 },
-        { name: "month",  value: 60 * 60 * 24 * 30 },
-        { name: "day",    value: 60 * 60 * 24 },
-        { name: "hour",   value: 60 * 60 },
-        { name: "minute", value: 60 }
-    ];
-
-    for (i=0; i<units.length; i++) {
-        
-        const amount = parseInt(seconds / units[i].value);
-        if (amount > 0)
-            if (amount === 1) 
-                return '1 ' + units[i].name + ' ago'
-            else    
-                return amount + ' ' + units[i].name + 's ago'
-    }
-    return "just now";
+    return dateString.split("T")[0];
 }
